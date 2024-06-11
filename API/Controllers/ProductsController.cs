c33211d195edc9344776237d7bd5ad8fecf803a6
shiva shankara <shans661@gmail.com>
2024-06-11 16:27:27 +0530

    BaseAPI controller added

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -9,9 +9,7 @@ using AutoMapper;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> ProductRepo;
         private readonly IGenericRepository<ProductBrand> ProductBrandRepo;
--- API/Program.cs
+++ API/Program.cs
@@ -27,7 +27,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseStaticFiles();
+app.UseStaticFiles(); 
 
 app.UseHttpsRedirection();
 
