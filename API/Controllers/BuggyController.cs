2a98f7570f977fd22e5731da20598efee5bbde6e
shiva shankara <shans661@gmail.com>
2024-06-12 17:03:06 +0530

    ApiResponse class added

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Errors;
 using Core.Entities;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -22,7 +23,7 @@ namespace API.Controllers
             Product item = Context.Products.Find(99);
             if(item == null)
             {
-                return NotFound();
+                return NotFound(new ApiResponse(404));
             }
             return Ok();
         }
@@ -39,7 +40,7 @@ namespace API.Controllers
         [HttpGet("badrequest")]
         public ActionResult GetBadRequest()
         {
-            return BadRequest();
+            return BadRequest(new ApiResponse(400));
         }
          [HttpGet("badrequest/{id}")]
         public ActionResult GetBadRequest(int id)
