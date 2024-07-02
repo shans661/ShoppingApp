8899bc8a17e250a187eb45b6393339bfe6c09d1f
shiva shankara <shans661@gmail.com>
2024-07-02 11:02:33 +0530

    Extension method added for adding services

--- API/Program.cs
+++ API/Program.cs
@@ -5,34 +5,13 @@ using Microsoft.EntityFrameworkCore;
 using API.Middlewares;
 using Microsoft.AspNetCore.Mvc;
 using API.Errors;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<StoreContext>(x => {
-    x.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-builder.Services.Configure<ApiBehaviorOptions>(options => {
-    options.InvalidModelStateResponseFactory = actionContext => {
-        var errors = actionContext.ModelState.Where(e => e.Value.Errors.Count >= 0)
-        .SelectMany(x => x.Value.Errors)
-        .Select(x => x.ErrorMessage).ToArray();
-
-        var errorResponse =  new ApiValidationErrorResponse{
-            Errors = errors
-        };
-
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
+builder.Services.AddApplicationServiceExtensions(builder.Configuration);
 
 var app = builder.Build();
 
